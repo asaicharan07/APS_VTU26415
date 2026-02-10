1bb561708383d1db7de5b92b19ad931215c9313e
asaicharan07 <vtu26415@veltech.edu.in>
2026-02-10 10:09:57 +0530

    Time: 1 ms (99.74%) | Memory: 47.6 MB (86.83%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int x = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[x++] = nums[i];
+            }
+        }
+        while (x < nums.length) {
+            nums[x++] = 0;
+        }
+    }
+}
